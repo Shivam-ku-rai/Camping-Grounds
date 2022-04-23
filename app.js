d9578ecd0dca341ac58971531e78cf1b6b05f3a2
shivam <raishivam3434@gmail.com>
2022-04-24 01:23:15 +0530

    Added basuc styles

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/camping-arena');
@@ -13,13 +15,16 @@ db.once("open", () => {
 
 const app = express();
 
-
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
 app.get('/',(req,res)=>{
     res.render('home');
-})
+});
 
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
